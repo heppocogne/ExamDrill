6ae8ab300a2eb12b51506bd7c812a9d77d9987df
heppocogne <83043568+heppocogne@users.noreply.github.com>
2026-04-10 14:34:47 +0900

    Update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.github.heppocogne.examdrill"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
